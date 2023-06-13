f3f21a1116b06e98fcfc67441e5cec81f0ae4d09
Risav03 <needleweed03@gmail.com>
2023-06-13 19:50:47 +0530

    all done

--- air-purifier/src/components/open.jsx
+++ air-purifier/src/components/open.jsx
@@ -4,10 +4,10 @@ import heading from "../content/heading.png"
 function Open(){
     return <div>
         <div className="shadow-xl rounded-3xl mx-auto">
-            <img src={heading} className="w-[15%] mx-auto"></img>
+            <img src={heading} className="md:w-[15%] w-[40%] mx-auto"></img>
         </div>
         <div className="grid md:grid-flow-col md:grid-cols-2 md:mt-[3rem] mt-10">
-            <img src={Product} className="w-[80%] max-[768px]:w-[90%] shadow-xl mx-auto rounded-3xl my-auto"></img>
+        <iframe className="rounded-3xl md:w-[90%] md:h-[23rem] h-[15rem] w-[90%] mx-auto md:mt-28" src="https://www.youtube.com/embed/6S6Do6MD4X4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             <div>
                 <div className="text-center bg-blue-100 rounded-3xl md:w-[80%] w-[90%] mt-10 mx-auto font-montserrat md:p-10 pt-5 px-4 pb-5 shadow-xl">
                     <h1 className="md:text-[3vw] text-blue-500 text-[10vw] font-bold">Air Quality Regulator</h1>
--- air-purifier/src/components/team.jsx
+++ air-purifier/src/components/team.jsx
@@ -13,6 +13,7 @@ function Team(){
             <div className="bg-blue-300 rounded-3xl w-[65%] sm:w-[85%] mx-auto p-6 border-[2px] border-blue-500 shadow-2xl">
                 <img alt="Soumanka Sarkar" src={Manka} className="w-[60%] mx-auto rounded-full border-[3px] border-white"></img>
                 <h3 className="text-blue-500 font-bold md:text-[1.3vw] mt-4 text-[4.5vw]">Soumanka Sarkar</h3>
+                <h4 className="text-white font-semibold text-[4vw] md:text-[1.2vw]">CEO</h4>
                 <a href="https://www.linkedin.com/in/soumanka-sarkar-012762232/"><img src={linkedIn} className="w-[15%] mx-auto mt-2"></img></a>
             </div>
 
@@ -20,12 +21,14 @@ function Team(){
             <div className="bg-blue-300 rounded-3xl w-[65%] sm:w-[85%] mx-auto p-6 border-[2px] border-blue-500 shadow-2xl">
                 <img alt="Soumanka Sarkar" src={meye2} className="w-[60%] mx-auto rounded-full border-[3px] border-white"></img>
                 <h3 className="text-blue-500 font-bold md:text-[1.3vw] mt-4 text-[4.5vw]">Anushka Ganguly</h3>
+                <h4 className="text-white font-semibold text-[4vw] md:text-[1.2vw]">COO</h4>
                 <a href="https://www.linkedin.com/in/anushka-ganguly-b3b1bb25a/?originalSubdomain=in"><img src={linkedIn} className="w-[15%] mx-auto mt-2"></img></a>
             </div>
 
             <div className="bg-blue-300 rounded-3xl w-[65%] sm:w-[85%] mx-auto p-6 border-[2px] border-blue-500 shadow-2xl">
                 <img alt="Soumanka Sarkar" src={meye1} className="w-[60%] mx-auto rounded-full border-[3px] border-white"></img>
                 <h3 className="text-blue-500 font-bold md:text-[1.3vw] mt-4 text-[4.5vw]">Aahana Chowdhury</h3>
+                <h4 className="text-white font-semibold text-[4vw] md:text-[1.2vw]">CTO</h4>
                 <a href="https://www.linkedin.com/in/aahana-chowdhury-522837274"><img src={linkedIn} className="w-[15%] mx-auto mt-2"></img></a>
             </div>
 
